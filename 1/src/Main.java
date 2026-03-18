7bae7ca3fae08b483d98be74b01e8507934aa935
Bartosz Rożnowicz <s33876@pjwstk.edu.pl>
2026-03-18 18:02:03 +0100

    Add input validation

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -2,7 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many numbers?");
+        int n = scanner.nextInt();
+
+        int[] values = new int[n];
+
         System.out.println("Enter numbers:");
+
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        double average = StatisticsHelper.calculateAverage(values);
+        System.out.println("Average: " + average);
     }
 }
--- 1/src/StatisticsHelper.java
+++ 1/src/StatisticsHelper.java
@@ -1,5 +1,8 @@
 public class StatisticsHelper {
     public static double calculateAverage(int[] values) {
+        if (values.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty");
+        }
         int sum = 0;
         for (int v : values) {
             sum += v;
