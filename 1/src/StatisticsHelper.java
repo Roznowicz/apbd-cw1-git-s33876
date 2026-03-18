8fcc7d546f5703087d30be16a74f4826b46d1cd2
Bartosz Rożnowicz <s33876@pjwstk.edu.pl>
2026-03-18 17:56:34 +0100

    Implement average calculation

--- 1/src/StatisticsHelper.java
+++ 1/src/StatisticsHelper.java
@@ -1,2 +1,9 @@
 public class StatisticsHelper {
+    public static double calculateAverage(int[] values) {
+        int sum = 0;
+        for (int v : values) {
+            sum += v;
+        }
+        return (double) sum / values.length;
+    }
 }
